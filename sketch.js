f4c974d3307ee92406aedb4578301c1e203d2dfb
TonyBru <42678997+TonyBru@users.noreply.github.com>
2018-08-24 18:32:58 +0200

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let slideNumber = 0;
+let sliderCount = 1;
+let slides;
+let slideTime = 6000;
+
+$(document).ready(function() {
+  sliderCount = document.getElementsByClassName('slide').length;
+  slides = document.getElementsByClassName('slide');
+  console.log(sliderCount);
+  window.setInterval(slide, slideTime);
+  window.setInterval(playVid, slideTime);
+
+
+  function slide() {
+    slideNumber++;
+    let left = -(slideNumber % sliderCount)*100;
+    $("#container").css('left', left + 'vw');
+  }
+
+  var videos = document.getElementsByTagName('video');
+  function playVid() {
+    for (let i = 0; i < videos.length; i++) {
+      if($.contains(slides[slideNumber % sliderCount], videos[i]) && videos[i].currentTime == 0){
+        videos[i].play();
+      }else if(!$.contains(slides[slideNumber % sliderCount], videos[i])) {
+        videos[i].pause();
+        videos[i].currentTime = 0;
+      }
+    }
+  }
+
+});
