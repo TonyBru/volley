c05c2ac05df3593928e72069e383ebf1d8e5c210
TonyBru <42678997+TonyBru@users.noreply.github.com>
2018-11-09 21:36:23 +0100

    Create kovvTables.js

---
+++ kovvTables.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 6; i++) {
+  $('table tr').find('td:eq(3),th:eq(3)').remove();
+}
+$('td').css('font-size', '18px');
+
+$('div').each(function (){
+  $('div').css('width', '100%!important');
+  $('div').css('padding', '0');
+});
+
